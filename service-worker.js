4ce06912b65617c786ab21f7845c88047707fe83
mathiarasi <mathiarasi@lingapos.com>
2024-09-17 23:05:01 +0530

    Updates

---
+++ service-worker.js
@@ -0,0 +1,9 @@
+// public/service-worker.js
+self.addEventListener('install', (event) => {
+    console.log('Service worker installing...');
+  });
+  
+  self.addEventListener('activate', (event) => {
+    console.log('Service worker activating...');
+  });
+  
